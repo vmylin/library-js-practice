c56f693d6945e63cd1a9ce6e586e6bc55a2a0c01
Victor Lin <vlinmy06@gmail.com>
2019-10-15 16:33:00 -0700

    close function is working, added comments to help with the borrow part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const automated = document.getElementById("automated");
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const bookId = document.getElementById('id');
+
 // Object Literal
 const library = {
     open: true,
@@ -9,14 +14,37 @@ const library = {
     ],
     lent: [],
     borrow: function (id) {
-    
+        // is open
+        
+        // input number
+        // click event
+        // put into lent[]
+        this.lent.push(this.inventory[id]) ;
     },
     returnItem: function (id) {
-        
+
     },
     close: function () {
-        
-        
+        const door = document.getElementById('door');
+        door.addEventListener('click', function () {
+            if (this.open == false) {
+                door.setAttribute('value', 'open');
+                console.log('t1');
+                this.open = true;
+            } else {
+                door.setAttribute('value', 'closed');
+                this.open = false;
+                console.log('t2');
+            }
+        });
     },
-    
 };
+// CODE BELOW IS IMPORTANT DONT DELETE
+// const p = document.createElement("p");
+// p.innerText = ('Sorry the library is closed');
+// automated.appendChild(p);
+
+
+
+console.log(library.inventory[0].author)
+library.close();
