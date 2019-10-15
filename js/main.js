e887a1c36969bdeac4df07f08c36f19812f30849
Victor Lin <vlinmy06@gmail.com>
2019-10-15 10:01:51 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Object Literal
+const library = {
+    open: true,
+    name: 'My Library',
+    inventory: [
+        { title: '1984', author: 'Orwell', id: 1 },
+        { title: 'Dune', author: 'Herbert', id: 2 },
+        { title: '1Q84', author: 'Murakami', id: 3 },
+    ],
+    lent: [],
+    borrow: function (id) {
+    
+    },
+    returnItem: function (id) {
+        
+    },
+    close: function () {
+        
+        
+    },
+    
+};
